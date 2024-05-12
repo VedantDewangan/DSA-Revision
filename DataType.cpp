f078f238f8837506ed00704d221078d4de8a1a82
VedantDewangan <135214899+VedantDewangan@users.noreply.github.com>
2024-05-12 23:30:25 +0530

    Add files via upload

---
+++ DataType.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // String
+    // size n byte and double
+    string name;
+    name = "Vedant";
+    cout << name << endl;
+
+    // Character
+    // size 1 byte and single
+    char gender = 'M';
+    cout << gender << endl;
+
+    // Integer
+    // size 4 byte
+    int marks = 98;
+    cout << marks << endl;
+
+    // Float
+    // size 4 byte
+    float CPI = 8.52;
+    cout << CPI << endl;
+
+    // Double
+    // size 8 byte
+    double pi = 3.14159265358979323846;
+    cout << pi << endl;
+
+    // Boolean
+    // size 1 byte
+    bool male = true;
+    cout << male << endl;
+
+    return 0;
+}
