4c6ca059fff6c273be31eb8d35e2df53a6f20c60
VedantDewangan <135214899+VedantDewangan@users.noreply.github.com>
2024-05-21 23:45:40 +0530

    Create 16_Patterns_1.cpp

---
+++ Basic C++/16_Patterns_1.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // // square
+    // // * * *
+    // // * * *
+    // // * * *
+    // int side;
+    // cout<<"Enter the number of side : ";
+    // cin>>side;
+    // for(int i=1;i<=side;i++){
+    //     for(int j=1;j<=side;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // // reactangle
+    // // * * * * *
+    // // * * * * *
+    // // * * * * *
+    // // * * * * *
+    // int row;
+    // cout<<"Enter number of rows : ";
+    // cin>>row;
+
+    // int col;
+    // cout<<"Enter number of column : ";
+    // cin>>col;
+
+    // for(int i=1;i<=row;i++){
+    //     for(int j=1;j<=col;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // //hollow square
+    // // * * * *
+    // // *     *
+    // // *     *
+    // // * * * *
+    // int side;
+    // cout<<"Enter the number of side : ";
+    // cin>>side;
+
+    // for(int i=1;i<=side;i++){
+    //     for(int j=1;j<=side;j++){
+    //         if(j==1 || i==1 || j==side || i==side){
+    //             cout<<"* ";
+    //         }
+    //         else{
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    // hollow reactangle
+    // * * * * *
+    // *       *
+    // * * * * *
+    // int row;
+    // cout<<"Enter number of row : ";
+    // cin>>row;
+
+    // int col;
+    // cout<<"Enter number of column : ";
+    // cin>>col;
+
+    // for(int i=1;i<=row;i++){
+    //     for(int j=1;j<=col;j++){
+    //        if(i==1 || j==1 || i==row || j==col){
+    //          cout<<"* ";
+    //        }
+    //        else{
+    //         cout<<"  ";
+    //        }
+    //     }
+    //     cout<<endl;
+    // }
+
+    // half pyrmid
+
+    // int side ;
+    // cout<<"Enter the number of side : ";
+    // cin>>side ;
+
+    // for(int i=1;i<=side;i++){
+    //     for(int j=1;j<=side;j++){
+    //         if(j<=i){
+    //             cout<<"* ";
+    //         }
+    //         else{
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    // inverted half prymid
+    // int side ;
+    // cout<<"Enter the number of side : ";
+    // cin>>side ;
+
+    // for(int i=1;i<=side;i++){
+    //     for(int j=1;j<=side;j++){
+    //         if(j<=side-i+1){
+    //             cout<<"* ";
+    //         }
+    //         else{
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    // hollow half prymid
+    // int side;
+    // cout << "Enter the number of side : ";
+    // cin >> side;
+
+    // for (int i = 1; i <= side; i++)
+    // {
+    //     for (int j = 1; j <= side; j++)
+    //     {
+    //         if (j == i || j==1 || i==side)
+    //         {
+    //             cout << "* ";
+    //         }
+    //         else
+    //         {
+    //             cout << "  ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    // hollow inverted half prymid
+    // int side;
+    // cout << "Enter the number of side : ";
+    // cin >> side;
+
+    // for (int i = 1; i <= side; i++)
+    // {
+    //     for (int j = 1; j <= side; j++)
+    //     {
+    //         if (j <= side - i + 1 || i == 1 || j == 1)
+    //         {
+    //             cout << "* ";
+    //         }
+    //         else
+    //         {
+    //             cout << "  ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    return 0;
+}
